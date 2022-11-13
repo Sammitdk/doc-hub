6e47537c6f4d3353a9f289b54cfa1e833fe76604
Sammit <sammitkhade77@gmail.com>
2022-11-13 13:35:46 +0530

    added api keys

---
+++ src/config.jsx
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: "AIzaSyCfqQbJeIUvpRGjBBbyRNDcnAcUtT6ocM8",
+  authDomain: "doc-hub-1a597.firebaseapp.com",
+  projectId: "doc-hub-1a597",
+  storageBucket: "doc-hub-1a597.appspot.com",
+  messagingSenderId: "1024125966204",
+  appId: "1:1024125966204:web:99cda38977968e85f5b6f9",
+  measurementId: "G-9SSWZLCV9M",
+};
+
+export default config;
